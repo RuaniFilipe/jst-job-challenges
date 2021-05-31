d38665597f1297ac20287fe7110382aaa0ab4a02
Ruâni Filipe <dev.ruanifilipe@gmail.com>
2021-05-31 16:02:47 -0300

    refactor phase1

---
+++ desafiojusta/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './Pages/Home'
+import GlobalStyles from './Styles/GlobalStyles';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Home />
+    </>
+  );
+}
+
+export default App;
